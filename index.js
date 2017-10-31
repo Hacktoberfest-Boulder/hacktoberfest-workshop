c3ef648ad3a5e55acd0cc8af509f49ee6330bc5d
Adam Smith <asmith13194@gmail.com>
2017-10-30 18:56:55 -0600

    add events list and add jquery closes #8 close #9

---
+++ index.js
@@ -0,0 +1,32 @@
+$( document ).ready(() => {
+  const events = [
+    {
+      name: 'hackathon',
+      date: '10/30/17'
+    },
+    {
+      name: 'skipping school',
+      date: '10/31/17'
+    },
+    {
+      name: 'christmas',
+      date: '12/25/17'
+    },
+    {
+      name: 'ny2e',
+      date: '12/31/17'
+    },
+    {
+      name: 'adams bday',
+      date: '01/31/17'
+    },
+    {
+      name: 'something fun',
+      date: '11/27/17'
+    },
+  ];
+ 
+  for (var i = 0; i < events.length; i++) {
+    $('.event-list').append($('<li>'+events[i].name + ' - ' + events[i].date+'</li>'))
+  }
+});
